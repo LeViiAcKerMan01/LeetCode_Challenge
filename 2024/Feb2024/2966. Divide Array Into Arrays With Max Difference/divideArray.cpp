3b6196dd7283e44e9bdf265d5dc627af2569305c
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-02-01 23:05:03 +0530

    First commit

---
+++ 2024/Feb2024/2966. Divide Array Into Arrays With Max Difference/divideArray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+
+        int n = nums.size();
+        int m = n/3;
+        int l = 1;
+        int j = 0;
+        int g = 3;
+
+        
+
+            for( int i = j; i < n; i += 3){
+
+                if(nums[i+2] - nums[i] > k){
+                    return{};
+                    // to return without doing anything!!
+                }
+                result.push_back({nums[i], nums[i+1], nums[i+2]});
+                // To push three element in one go in a 2D vector
+            }
+        return result;
+    }
+};
